f7a5b849bb0d6f8d238f9115254ab91e3b88156e
jhumer-apus <jhumerapus@gmail.com>
2023-03-20 23:36:17 +0800

    updated

--- client/components/Login.vue
+++ client/components/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="form-wrapper">
-    <form>
+    <form class="login-form">
       <h1>Log In To Your Account</h1><br><br>
       <input type="text" id="username" name="username" placeholder="Username"><br><br>
       <input type="text" id="username" name="username" placeholder="Password"><br><br>
--- client/pages/registration.vue
+++ client/pages/registration.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="registration-container">
-        <div class="form-wrapper m-auto bg-black">
+        <div class="form-wrapper m-auto">
             <h1 class="text-center text-2xl">eDokumento</h1><br>
             <form>
                 <p class="text-center">Step 1 of 2</p><br><br><br>
-                <input type="text" id="firstname" name="firstname" placeholder="First Name"><br><br>
-                <input type="text" id="middlename" name="middlename" placeholder="Middle Name"><br><br>
-                <input type="text" id="lastname" name="lastname" placeholder="Last Name"><br><br>
-                <div class="select-container">
+                <input type="text" id="firstname" name="firstname" placeholder="First Name" required><br><br>
+                <input type="text" id="middlename" name="middlename" placeholder="Middle Name" required><br><br>
+                <input type="text" id="lastname" name="lastname" placeholder="Last Name" required><br><br>
+                <div class="select-container" required>
                     <select id="gender" name="gender" placeholder="Gender">
                         <option value="" disabled selected hidden>Gender</option>
                         <option value="male">Male</option>
@@ -15,14 +15,25 @@
                         <option value="non-binary">Non-binary</option>
                         <option value="others">Not to mention</option>
                     </select>
-                    <select id="civil_status" name="civil_Status" placeholder="Civil Status">
+                    <select id="civil_status" name="civil_Status" placeholder="Civil Status" required>
                         <option value="" disabled selected hidden>Civil Status</option>
                         <option value="single">Single</option>
                         <option value="non-binary">Married</option>
                     </select>
                 </div><br>
-                <input type="date" class="w-full">
-            </form>
+                <label>Birthdate</label>
+                <input type="date" class="w-full"><br><br>
+                <label>Mother's Maiden Name</label>
+                <div class="input-container">
+                    <input type="text" id="firstname" name="firstname" placeholder="First Name" required>
+                    <input type="text" id="firstname" name="firstname" placeholder="Middle Name" required>
+                    <input type="text" id="firstname" name="firstname" placeholder="Laste Name" required>
+                </div>
+                <div class="button-wrapper text-center">
+                    <button class="py-4 bg-sky-900 rounded-full w-96 m-auto font-bold text-white text-2xl">Next</button>
+                </div>
+            </form><br>
+            
     </div>
 </div>
   
