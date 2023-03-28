a96d454f60adc2c09f87312ecd71497d90c03a13
jhumer-apus <jhumerapus@gmail.com>
2023-03-28 08:58:06 +0800

    updated

--- client/components/HeaderUser.vue
+++ client/components/HeaderUser.vue
@@ -1,7 +1,11 @@
 <template>
-    <div class="sticky top-0 bg-sky-700 px-4">
-        <div class="py-4 text-2xl">
-            <font-awesome-icon :icon="['fas', 'bars']" />
+    <div class="sticky top-0 bg-sky-700 pl-4 pr-8 flex justify-between items-center h-16 py-4">
+        <div class="text-2xl">
+            <font-awesome-icon :icon="['fass', 'bars']" size="xl" />
+        </div>
+        <div class="flex items-center">
+            <img src="~assets/images/id_nako.jpg" class="rounded-full" width="50" height="50">
+            <span class="text-xl ml-4">Jhumer Apus</span>
         </div>
     </div>
   
