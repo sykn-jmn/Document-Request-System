e665e5a0c55c156b2dca1991c2e70b90c25dcf8f
jhumer-apus <jhumerapus@gmail.com>
2023-03-30 23:15:30 +0800

    updated

--- client/pages/[user]/request.vue
+++ client/pages/[user]/request.vue
@@ -7,19 +7,19 @@
                 <div class="request_container">
                     <h2>APPLICATION FORM</h2>
                     <h3>Application form will be generated based on your profile in web-app</h3>
-                    <button>GENERATE</button><span>Succesfully generated</span><br>
-                    <button>VIEW</button>
+                    <button>GENERATE</button><span class="status">Succesfully generated</span><br>
+                    <button class="isButtonEnabled">VIEW</button>
                 </div>
                 <div class="request_container">
                     <h2>CEDULA</h2>
                     <h3>Cedula will be generated based on your profile in web-app</h3>
-                    <button>GENERATE</button><span>Succesfully generated</span><br>
-                    <button>VIEW</button>
+                    <button>GENERATE</button><span class="status">Succesfully generated</span><br>
+                    <button :class="state.buttonStatus" disable>VIEW</button>
                 </div>
                 <div class="request_container">
                     <h2>PUROK CLEARANCE</h2>
                     <h3>Upload a photo/scanned copy of your recent purok clearance. Valid file types are <b>JPG</b> and <b>PDF</b> only</h3>
-                    <input type="file" class="outline:none">
+                    <input type="file">
                 </div>
             </div>
         </div>
@@ -34,6 +34,11 @@ export default {
          definePageMeta({
             layout: 'user'
         });
+
+        const state = reactive({
+            buttonStatus: "isButtonDisabled"
+        })
+        return{state}
     }
 
 }
