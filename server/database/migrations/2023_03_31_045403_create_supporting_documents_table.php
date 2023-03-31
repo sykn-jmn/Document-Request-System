5518caca28d1201de4d6f9f299222e98bafa6f44
jhumer-apus <jhumerapus@gmail.com>
2023-03-31 23:07:28 +0800

    created migrations

---
+++ server/database/migrations/2023_03_31_045403_create_supporting_documents_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('supporting_documents', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('request_id')->index();
+            $table->string('filename');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('supporting_documents');
+    }
+};
---
+++ server/database/migrations/2023_03_31_142025_create_requests_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('code');
+            $table->bigInteger('user_id')->index();
+            $table->date('date_requested');
+            $table->string('purpose');
+            $table->bigInteger('supporting_document_id')->index();
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('requests');
+    }
+};
