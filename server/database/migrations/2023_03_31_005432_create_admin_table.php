f28bee9529bee245fb60e76efb973c6ca0774bdb
jhumer-apus <jhumerapus@gmail.com>
2023-03-31 08:55:27 +0800

    created migration user and admin

---
+++ server/database/migrations/2023_03_31_005432_create_admin_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admin', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
+            $table->date('birthdate');
+            $table->enum('gender',['male','female','non-binary','not to mention']);
+            $table->enum('civil_status',['single','married']);
+            $table->string('mobile_number');
+            $table->string('mothers_firstname');
+            $table->string('mothers_middlename');
+            $table->string('mothers_lasttname');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admin');
+    }
+};
