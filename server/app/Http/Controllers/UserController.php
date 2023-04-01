f62e088467b96727719c8b553d641b388ef1f7f7
jhumer-apus <jhumerapus@gmail.com>
2023-04-01 17:27:09 +0800

    updated

--- server/app/Http/Controllers/UserController.php
+++ server/app/Http/Controllers/UserController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Modules\Users;
+use App\Modules\UserLogin;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function login(Users $userLogin, Request $request){
+    public function login(UserLogin $userLogin, Request $request){
         return $userLogin->login($request);
     }
 }
