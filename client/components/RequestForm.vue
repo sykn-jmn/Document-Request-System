1a51d98eac5404fd67f2d5489d095b9a6db7ff0b
jhumer-apus <jhumerapus@gmail.com>
2023-04-25 23:34:57 +0800

    updated

--- client/components/RequestForm.vue
+++ client/components/RequestForm.vue
@@ -3,17 +3,49 @@
         <h1 class="text-3xl font-bold">Complete the Request Form</h1>
         <p class="font-medium">Make sure to not leave any field blank</p><br>
         <form>
-            <div class="col-span-2">
+            <div class="detail-container col-span-2">
                 <p class="label">Full Name<span class="guide"> (First Name M.I. Last Name)</span></p>
                 <div class="box">
                     {{data.first_name + " " + data.middle_initial + ". " + data.last_name}}
                 </div>
             </div>
-            <div>
+            <div class="detail-container">
                 <p class="label">Age</p>
                 <div class="box">
-                    {{data.first_name + " " + data.middle_initial + ". " + data.last_name}}
+                    {{getAge(data.birthdate)}}
                 </div>
+            </div>
+            <div class="detail-container">
+                <p class="label">Birthdate <span class="guide">(yyyy-mm-dd)</span></p>
+                <div class="box">
+                    {{data.birthdate}}
+                </div>
+            </div>
+            <div class="detail-container col-span-2">
+                <p class="label">Complete Address<span class="guide"> (Street/Purok, Baranggay, City/Municipality, Province)</span></p>
+                <div class="box">
+                    {{data.purok + " " + data.baranggay + " " + data.municipality + " " + data.province}}
+                </div>
+            </div>
+            <div class="detail-container">
+                <p class="label">Email</p>
+                <div class="box">
+                    {{data.email}}
+                </div>
+            </div>
+            <div class="detail-container">
+                <p class="label">Mobile Number</p>
+                <div class="box">
+                    {{data.mobile_number}}
+                </div>
+            </div>
+            <label class="col-span-2">
+                Purpose of Request
+                <textarea class="mt-4" v-model="purposeOfRequest"></textarea>
+            </label>
+            <div class="detail-container col-span-2">
+                <p class="label">Supporting Documents</p>
+                <span class="guide">Upload photo/scanned copy of your supporting documents.</span>
             </div>
         </form>
     </div>
@@ -23,7 +55,8 @@
 export default {
     data(){
         return{
-            data:''
+            data:'',
+            purposeOfRequest:'',
         }
     },
     mounted(){
@@ -35,6 +68,18 @@ export default {
                 this.data = response.data
                 this.data.middle_initial = response.data.middle_name[0]
             })
+        },
+        getAge(birthdate){
+
+                var today = new Date();
+                var birthDate = new Date(birthdate);
+                var age = today.getFullYear() - birthDate.getFullYear();
+                var m = today.getMonth() - birthDate.getMonth();
+                if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+                    age--;
+                }
+                return age;
+
         }
     }
 
@@ -42,8 +87,8 @@ export default {
 </script>
 
 <style scoped>
-.label{
-    @apply font-semibold text-xl
+.label, label{
+    @apply font-semibold text-xl mb-4
 }
 .guide{
     @apply text-slate-400 font-normal
@@ -53,6 +98,9 @@ export default {
 }
 form{
     @apply grid grid-cols-2 gap-4 bg-white p-8 md:rounded-xl
+}
+.detail-container{
+    @apply mb-4
 }
 
 </style>
---
+++ client/pages/login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="index-container">
+            <AppHeader />
+            <div class="home">
+                <div class="description-title text-right">
+                    <h1><span class="text-yellow-400">Make</span> your document <span class="text-yellow-400">request faster</span></h1><br>
+                    <p class="font-medium">Save your time and have a hassle free request of branggay documents.</p>
+                </div>
+                <Login />
+            </div>
+        </div>
+        <AppFooter />
+    </div>
+    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
