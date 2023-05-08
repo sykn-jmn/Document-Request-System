198e3b8f382a77ba4e04cb8ebff8f073924b1161
jhumer-apus <jhumerapus@gmail.com>
2023-05-08 08:58:01 +0800

    updated

---
+++ client/components/RegisterResidentsTable.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <table class="table-history">
+        <thead class="border-y text-left">
+            <th>Full Name</th>
+            <th>Registration Date</th>
+            <th>Sex</th>
+            <th>Contact Information</th>
+            <th>Address</th>
+        </thead>
+   
+    <tr v-for="resident in data" :key="resident.id">
+        <td>{{resident.first_name}} {{resident.last_name}}</td>
+        <td>{{resident.email_verified_at}}</td>
+        <td>
+            <div>
+                <p>{{resident.email}}</p>
+                <p>{{resident.mobile_number}}</p>
+            </div>
+        </td>
+        <td>{{resident.purok+" "+resident.branggay+" "+resident.municipality+" "+resident.province}}</td>
+    </tr>
+
+    </table>
+    <div>
+        <span></span>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+export default {
+    props:['data'],
+    data(){
+        return{
+        }
+    },
+    methods:{
+
+    }
+
+}
+</script>
+
+<style scoped>
+table{
+    border-collapse: separate;
+    border-spacing: 0 8px; 
+    @apply w-full
+}
+th{
+    @apply p-8
+}
+tr{
+    background:white;
+}
+td{
+    @apply w-fit p-8
+}
+.pending{
+    @apply bg-yellow-400 w-fit text-white px-8 py-2 rounded-2xl
+}
+.rejected{
+    @apply bg-red-500 w-fit text-white px-8 py-2 rounded-2xl
+}
+.approved{
+    @apply bg-green-400 w-fit text-white px-8 py-2 rounded-2xl
+}
+.completed{
+    @apply bg-blue-500 w-fit text-white px-8 py-2 rounded-2xl
+}
+.processing{
+    @apply bg-stone-500 w-fit text-white px-8 py-2 rounded-2xl
+}
+</style>
---
+++ client/pages/admin/registered-residents/_index.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="main-container">
+    <h1>Register Residents</h1>
+    <p class="addUser" @click="User">+ ADD USER</p><br>
+    <Search 
+      @search="search" 
+      :selected="$route.params.index" 
+      :options="options" 
+      placeholder="Search Residents"
+    />
+    <RegisterResidentsTable :data="data"/>
+    <Pagination 
+      :currentPage="currentPage"
+      :lastPage="lastPage"
+      @paginate="paginate"
+    />
+    <Spin v-if="spinning"/>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+export default {
+    layout: 'admin',
+    data(){
+      return{
+        data:[],
+        currentPage: 1,
+        lastPage:1,
+        spinning:false,
+        options:['all','completed','approved','processing','pending','rejected'],
+      }
+    },
+    mounted(){
+      this.fetchResidents(this.currentPage)
+    },
+    methods:{
+      newRequest(){
+
+      },
+      paginate(value){
+        switch(value){
+          case "prev":
+            var pageNumber = this.currentPage-1
+            break;
+          case"next":
+            var pageNumber = this.currentPage+1
+            break;
+          default:
+            var pageNumber = this.currentPage
+        }
+        this.fetchResidents(pageNumber,null)
+      },
+      async fetchResidents(pageNumber,search){
+        this.spinning = true
+        var params = {
+          search:search
+        }
+        await this.$axios.get('/admin/get-residents/'+this.$route.params.index+'?page=' + pageNumber, {params}).then(response=>{
+          this.data = response.data.data
+          this.currentPage = response.data.current_page
+          this.lastPage = response.data.last_page
+          this.spinning = false
+        }).catch(err=>{
+          this.spinning = false
+        })
+      },
+    search(value){
+      this.fetchResidents(1,value)
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+.main-container{
+  @apply pt-12 px-12 pb-12 text-black min-h-screen
+}
+h1{
+  @apply text-2xl font-bold text-black
+}
+.newRequest{
+  @apply text-right text-lg text-sky-700 font-bold
+}
+
+</style>
