37317e141b0c2d103320c5b2e46182422806aeec
jhumer-apus <jhumerapus@gmail.com>
2023-05-11 00:37:21 +0800

    updated

--- client/components/ViewPDF.vue
+++ client/components/ViewPDF.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-black">
-    <embed src="../../server/storage/app/public/supporting_documents/1683726470_loh.pdf" type="application/pdf" width="100%"/>
+
   </div>
 </template>
 
