53001cde43daf835e4558857463d09bfa48c1ad1
jhumer-apus <jhumerapus@gmail.com>
2023-05-11 09:00:26 +0800

    updated

--- client/components/ViewPDF.vue
+++ client/components/ViewPDF.vue
@@ -1,12 +1,24 @@
 <template>
-  <div class="bg-black">
-
+  <div>
+    <button @click="getLink">click</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return{
+      pdfsrc:''
+    }
+  },
+  methods:{
+    getLink(){
+      const blob = new Blob(['../../server/storage/app/public/supporting_documents/1683726470_loh.pdf'],{type: "application/pdf"})
+      const objectUrl = window.URL.createObjectURL(blob)
+      this.pdfsrc = objectUrl
+      window.open(objectUrl);
+    }
+  }
 }
 </script>
 
