7e3be1bf0dc3f2a96ddb50d02fc80eb919b5578a
jhumer-apus <jhumerapus@gmail.com>
2023-06-01 00:09:11 +0800

    updated

--- client/pages/another-test.vue
+++ client/pages/another-test.vue
@@ -1,81 +1,96 @@
 <template>
   <div>
-    <div id = "content" class="bg-blue-900 text-center bg-white text-black m-auto py-8">
-        <div class="flex items-center m-auto justify-around">
-            <img src="~/assets/images/Maranding_Logo.png" width="100" height="120"/>
-            <div class="text-base">
-                <p>Republic of the Philippines</p>
-                <p>Province of Lanao Del Norte</p>
-                <p>Municipality of<b> Lala</b></p>
-                <p>Baranggay <b>Maranding</b></p>
+        <button @click="generatePDF">Generate PDF</button>
+    <div id="content">
+        <img src="~/assets/images/DESIGN.jpg" class="block">
+        <div class="absolute top-0 right-0 pt-4">
+             <div class="flex items-center m-auto justify-around">
+                <br>
+                <div class="text-base text-white leading-tight">
+                    <p>Republic of the Philippines</p>
+                    <p>Province of Lanao Del Norte</p>
+                    <p>Municipality of<b> Lala</b></p>
+                    <p>Barangay <b>Maranding</b></p>
+                </div>
+                <br>
             </div>
-            <img src="~/assets/images/Maranding_Logo.png" width="100" height="120"/>
-        </div><br>
-        <h1>BARANGGAY CLEARANCE</h1><br><br>
-        <div class="grid grid-cols-3 gap-x-8">
-            <div class="pl-16">
-                <div class="par">
-                    <p class="officials_name">Hon. Jeoffy M.Fung</p>
-                    <p>Punong Baranggay</p>
-                    <p>Commitee on Transportation, Peace and Order, Disaster Risk Reduction and Management</p>
+            <h1>BARANGAY CLEARANCE</h1>
+            <div class="grid grid-cols-3 gap-x-8 bg-transparent mt-20">
+                <div class="pl-16">
+                    <div class="par">
+                        <p class="officials_name">Hon. Jeoffy M.Fung</p>
+                        <p>Punong Barangay</p>
+                        <p>Commitee on Transportation, Peace and Order, Disaster Risk Reduction and Management</p>
+                    </div>
+                    <div class="par">
+                        <p class="officials_name">Hon. Graciano S. Termis Jr.</p>
+                        <p>Commitee on Agriculture &amp; on Environmental Management</p>
+                    </div>
+                    <div class="par">
+                        <p class="officials_name">Hon. Roseller S. Fiel II</p>
+                        <p>Commitee on Education, Cultures and Arts, Tourism, Information and Communication Technology</p>
+                    </div>
+                    <div class="par">
+                        <p class="officials_name">Hon. Marites G. Procianos</p>
+                        <p>Commitee on Finance, Appropriations, Ways and Means &amp; on Cooperative</p>
+                    </div>
+                    <div class="par">
+                        <p class="officials_name">Hon. Lucky Jade G. de Guzman</p>
+                        <p>Commitee on Health, Sanitation, and Solid Waste Management</p>
+                    </div>
+                    <div class="par">
+                        <p class="officials_name">Hon. Arturo G. Arcillas</p>
+                        <p>Commitee on Laws, Rules, Legislative Relation, Municipal and Barangay Affairs</p>
+                    </div>
+                    <div class="par">
+                        <p class="officials_name">Hon. Peddy N. Baculao</p>
+                        <p>Commitee on Economic Enterprises, &amp; Trade and Industry</p>
+                    </div>
+                    <div class="par">
+                        <p class="officials_name">Hon. Allan S Guzman</p>
+                        <p>Committee on Ways and Means, Women and Children</p>
+                    </div>
+                    <div class="par">
+                        <p class="officials_name">Hon. John Michael Y Quilbrance</p>
+                        <p>Committee on Youth, Sports Devleopment Games and Amusement</p>
+                    </div>
+                    <div class="par">
+                        <p class="officials_name">Mr. Melvin C. Rabago</p>
+                        <p>Barangay Treasurer</p>
+                    </div>
+                    <div class="par">
+                        <p class="officials_name">Michelle Ann C. Masecampo</p>
+                        <p>Barangay Secretary</p>
+                    </div>
                 </div>
-                <div class="par">
-                    <p class="officials_name">Hon. Graciano S. Termis Jr.</p>
-                    <p>Commitee on Agriculture &amp; on Environmental Management</p>
-                </div>
-                <div class="par">
-                    <p class="officials_name">Hon. Roseller S. Fiel II</p>
-                    <p>Commitee on Education, Cultures and Arts, Tourism, Information and Communication Technology</p>
-                </div>
-                <div class="par">
-                    <p class="officials_name">Hon. Marites G. Procianos</p>
-                    <p>Commitee on Finance, Appropriations, Ways and Means &amp; on Cooperative</p>
-                </div>
-                <div class="par">
-                    <p class="officials_name">Hon. Lucky Jade G. de Guzman</p>
-                    <p>Commitee on Health, Sanitation, and Solid Waste Management</p>
-                </div>
-                <div class="par">
-                    <p class="officials_name">Hon. Arturo G. Arcillas</p>
-                    <p>Commitee on Laws, Rules, Legislative Relation, Municipal and Baranggay Affairs</p>
-                </div>
-                <div class="par">
-                    <p class="officials_name">Hon. Peddy N. Baculao</p>
-                    <p>Commitee on Economic Enterprises, &amp; Trade and Industry</p>
-                </div>
-                <div class="par">
-                    <p class="officials_name">Hon. Allan S Guzman</p>
-                    <p>Committee on Ways and Means, Women and Children</p>
-                </div>
-                <div class="par">
-                    <p class="officials_name">Hon. John Michael Y Quilbrance</p>
-                    <p>Committee on Youth, Sports Devleopment Games and Amusement</p>
-                </div>
-                <div class="par">
-                    <p class="officials_name">Mr. Melvin C. Rabago</p>
-                    <p>Baranggay Treasurer</p>
-                </div>
-                <div class="par">
-                    <p class="officials_name">Michelle Ann C. Masecampo</p>
-                    <p>Baranggay Secretary</p>
-                </div>
-            </div>
 
-            <div class="col-span-2 text-justify pr-16">
-                <h2>TO WHOM IT MAY CONCERN:</h2><br>
-                <p class="par_content">This is to certify that {{user.call_address}}. {{user.full_name}}, {{user.age}} years old {{user.civil_status}} is a bonafide resident of {{user.purok}}, Baranggay Maranding, Lala, Lanao Del Norte.</p><br>
+                <div class="col-span-2 text-justify pr-16 leading-normal">
+                    <h2>TO WHOM IT MAY CONCERN:</h2><br>
+                    <p class="par_content">This is to certify that {{user.call_address}}. {{user.full_name}}, {{user.age}} years old {{user.civil_status}} is a bonafide resident of {{user.purok}}, Barangay Maranding, Lala, Lanao Del Norte.</p><br>
 
-                <p class="par_content">This certifies further that as per records available in this office, {{user.pronouns}} have never been accused of any crime involving moral turpitude nor a member to any subversive anti-government social organization.</p><br>
+                    <p class="par_content">This certifies further that as per records available in this office, {{user.pronouns}} have never been accused of any crime involving moral turpitude nor a member to any subversive anti-government social organization.</p><br>
 
-                <p class="par_content">This certification is being issued upon the request of the said interested party in connection with his/her desire to/for:</p><br>
+                    <p class="par_content">This certification is being issued upon the request of the said interested party in connection with his/her desire to/for:</p><br>
 
-                <p class="font-semibold">"Requirement {{user.purpose}}"</p><br>
+                    <p class="font-bold">"Requirement {{user.purpose}}"</p><br>
 
-                <p class="par_content">Issued this <span class="font-semibold">{{currentDate()}} day of {{currentMonth()}}, {{currentYear()}}</span> at Barangay Maranding, Lala, Lanao del Norte, Philippines</p>
+                    <p class="par_content">Issued this <span class="font-bold">{{currentDate()}} day of {{currentMonth()}}, {{currentYear()}}</span> at Barangay Maranding, Lala, Lanao del Norte, Philippines</p><br>
+
+                    <div class="flex space-x-8">
+                        <div>
+                            <div class="thumb"></div>
+                            Left Thumb Mark
+                        </div>
+                        
+                        <div>
+                            <div class="thumb"></div>
+                            Right Thumb Mark
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-    <button @click="generatePDF">Generate PDF</button>
     <Spin v-if="spinning"/>
   </div>
 </template>
@@ -83,6 +98,7 @@
 <script>
 import moment from 'moment'
 import jsPDF from'jspdf'
+import backgroundImage from '~/assets/images/Maranding_Logo.png'
 export default {
     data(){
         return{
@@ -107,7 +123,9 @@ mounted(){
 methods:{
     async generatePDF(){
         this.spinning =  true
-        const doc = new jsPDF(undefined, undefined, 'a4');
+        const doc = new jsPDF('pt', 'mm', 'a4');
+        // doc.addImage(backgroundImage, 'PNG', 0, 0, 210, 297);
+
         var elementHTML = document.querySelector("#content");
 
         await doc.html(elementHTML, {
@@ -166,22 +184,27 @@ methods:{
     width:210mm;
     height:297mm;
     font-family: "Times New Roman", Times, serif;
-    line-height: normal;
+    @apply text-center text-black m-auto relative
 }
 .officials_name{
     @apply font-semibold text-sky-700
 }
 h1{
-    @apply font-bold text-3xl
+    font-weight: 900;
+    font-size:40px;
+    @apply text-black mt-16  text-center
 }
 h2{
     @apply font-semibold
 }
 .par{
-    
-    @apply m-auto text-xs mb-4 leading-tight; 
+    font-size:11px;
+    @apply m-auto mb-4 leading-tight; 
 }
 .par_content{
     @apply indent-10    
+}
+.thumb{
+    @apply border border-black w-28 h-28
 }
 </style>
