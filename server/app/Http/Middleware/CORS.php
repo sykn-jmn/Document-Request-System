fd2a28e6fd696be5bb8028fc4a8ab9deb8d73dda
Jeman Mama <jemanmama@gmail.com>
2023-06-10 23:05:23 +0800

    specified allowed origins

--- server/app/Http/Middleware/CORS.php
+++ server/app/Http/Middleware/CORS.php
@@ -15,7 +15,7 @@ class CORS
      */
     public function handle($request, Closure $next)
     {
-        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Origin: https://document-request-system-frontend.onrender.com");
         //ALLOW OPTIONS METHOD
         $headers = [
             'Access-Control-Allow-Methods' => 'POST,GET,OPTIONS,PUT,DELETE',
