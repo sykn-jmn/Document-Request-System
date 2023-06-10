ad80f697408fc028bc7bb9c721451393cc22095b
Jeman Mama <jemanmama@gmail.com>
2023-06-10 23:10:37 +0800

    Added allowed credentials in cors

--- server/app/Http/Middleware/CORS.php
+++ server/app/Http/Middleware/CORS.php
@@ -20,6 +20,7 @@ class CORS
         $headers = [
             'Access-Control-Allow-Methods' => 'POST,GET,OPTIONS,PUT,DELETE',
             'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization',
+            'Access-Control-Allow-Credentials' => 'true'
         ];
         if ($request->getMethod() == "OPTIONS"){
             //The client-side application can set only headers allowed in Access-Control-Allow-Headers
