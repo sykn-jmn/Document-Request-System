a487ba04c5ece7d2218e63bd3cdcd994879ecfa1
Jeman Mama <jemanmama@gmail.com>
2023-06-10 20:27:25 +0800

    specified dotenv path

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -34,6 +34,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
+    ['@nuxtjs/dotenv', { path: './' }]
   ],
 
   axios: {
